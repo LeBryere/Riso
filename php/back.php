9c1f68137639bad875a6da05b5cc2bd87b72a858
LeBryere <lebryere@gmail.com>
2024-06-06 12:17:13 +0200

    Törlés a távoli repóból

--- php/back.php
+++
@@ -1,8 +0,0 @@
-	<div id="back-to-top">
-		<div class="top" id="top">
-			<a id="pq-back-to-top" href="#" class="on">
-				<span class="pq-icon-up">
-					<i class="ion-ios-arrow-up"></i> </span>
-			</a>
-		</div>
-	</div>
